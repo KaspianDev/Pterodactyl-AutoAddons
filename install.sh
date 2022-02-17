0727c9de409f8d8976016d1448e6a50cb5b3fd50
Kacper Smoliński <34658474+KaspianDev@users.noreply.github.com>
2022-02-17 14:03:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,11 +78,11 @@ print_brake 70
 echo
 
 Backup() {
-bash <(curl -s https://raw.githubusercontent.com/kacperleague9/Pterodactyl-AutoAddons/main/backup.sh)
+bash <(curl -s https://raw.githubusercontent.com/KaspianDev/Pterodactyl-AutoAddons/main/backup.sh)
 }
 
 More_Buttons() {
-bash <(curl -s https://raw.githubusercontent.com/kacperleague9/Pterodactyl-AutoAddons/main/addons/version1.x/More_Buttons/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/KaspianDev/Pterodactyl-AutoAddons/main/addons/version1.x/More_Buttons/build.sh)
 }
 
 More_Server_Info() {
@@ -90,7 +90,7 @@ bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons
 }
 
 Server_Router_Icons() {
-bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons/${SCRIPT_VERSION}/addons/version1.x/Server_Router_Icons/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/KaspianDev/Pterodactyl-AutoAddons/main/addons/version1.x/Server_Router_Icons/build.sh)
 }
 
 PMA_Button_NavBar() {
@@ -106,11 +106,11 @@ bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoAddons
 }
 
 Bigger_Console() {
-bash <(curl -s https://raw.githubusercontent.com/kacperleague9/Pterodactyl-AutoAddons/main/addons/version1.x/Bigger_Console/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/KaspianDev/Pterodactyl-AutoAddons/main/addons/version1.x/Bigger_Console/build.sh)
 }
 
 Files_In_Editor() {
-bash <(curl -s https://raw.githubusercontent.com/kacperleague9/Pterodactyl-AutoAddons/main/addons/version1.x/Files_In_Editor/build.sh)
+bash <(curl -s https://raw.githubusercontent.com/KaspianDev/Pterodactyl-AutoAddons/main/addons/version1.x/Files_In_Editor/build.sh)
 }
 
 
